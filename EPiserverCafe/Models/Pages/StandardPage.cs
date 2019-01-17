5425ae38183ac62d050024a3e486d72b314c18a4
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-17 12:20:13 +0100

    - Added _Layout to the Shared folder with default layout - Created StartPageController.cs - Created two page types: StartPage & StandardPage

---
+++ EPiserverCafe/Models/Pages/StandardPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EPiserverCafe.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "ba858bd3-b813-4ab8-9a7d-2b3dbc538ea3", Description = "")]
+    public class StandardPage : PageData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
