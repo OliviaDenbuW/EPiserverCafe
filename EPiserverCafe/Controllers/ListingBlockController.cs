e24555f7be3978893e86331dded1a62d8b52abdc
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-18 10:53:36 +0100

    - Created ListingBlockController.cs ---------------------------------------------- - Added a constructor to get access to the loader (contentReference) - Created a viewModel and added values to heading from the incoming listing block (currentBlock) and filtered its children - Set the filtered value of children to the Pages property - Passed the viewModel to the partial view (which is not already created)

---
+++ EPiserverCafe/Controllers/ListingBlockController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Filters;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+using EPiserverCafe.Models.Blocks;
+using EPiserverCafe.Models.ViewModels;
+
+namespace EPiserverCafe.Controllers
+{
+    public class ListingBlockController : BlockController<ListingBlock>
+    {
+        private readonly IContentLoader Loader;
+
+        public ListingBlockController(IContentLoader loader)
+        {
+            Loader = loader;
+        }
+
+        public override ActionResult Index(ListingBlock currentBlock)
+        {
+            var viewModel = new ListingBlockViewModel()
+            {
+                Heading = currentBlock.Heading
+            };
+
+            if (currentBlock.ShowChildrenOfThisPage != null)
+            {
+                IEnumerable<PageData> children = Loader.GetChildren<PageData>(currentBlock.ShowChildrenOfThisPage);
+
+                // Remove pages:
+                // 1. that are not published
+                // 2. that the visitor does not have Read access to
+                // 3. that do not have a page template
+                IEnumerable<IContent> filteredChildren = FilterForVisitor.Filter(children);
+
+                // 4. that do not have "Display in navigation" selected
+                viewModel.Pages = filteredChildren.Cast<PageData>().Where(page => page.VisibleInMenu);
+            }
+
+            return PartialView(viewModel);
+        }
+    }
+}
---
+++ EPiserverCafe/Models/ViewModels/ListingBlockViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Core;
+using System.Collections.Generic;
+
+namespace EPiserverCafe.Models.ViewModels
+{
+    public class ListingBlockViewModel
+    {
+        public string Heading { get; set; }
+
+        public IEnumerable<PageData> Pages { get; set; }
+    }
+}
